10ebaaa6883d37096fe558e9c1ea3c39d92ec11d
gayathri mallisetti <gayathrimallisetti118@gmail.com>
2026-02-17 15:04:35 +0530

    Comment out Kubernetes provider in provider.tf
    Comment out Kubernetes provider configuration.

--- provider.tf
+++ provider.tf
@@ -4,10 +4,10 @@
 
 data "aws_eks_cluster_auth" "cluster" {
   name = local.cluster_name
-}
+}*/
 
 
-provider "kubernetes" {
+/*provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
