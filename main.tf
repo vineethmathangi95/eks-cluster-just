0dcf65232d85a629856b5b43db821d6d8636e469
vineeth mathangi <125802544+vineethmathangi95@users.noreply.github.com>
2026-03-05 01:59:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -105,3 +105,4 @@ module "irsa-ebs-csi" {
   provider_url                  = module.eks.oidc_provider
   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+}
