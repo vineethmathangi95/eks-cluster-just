4bcc3d1b1acaef6eda08b332b681952b4f15e326
vineeth mathangi <125802544+vineethmathangi95@users.noreply.github.com>
2026-03-18 14:54:00 +0530

    Remove commented-out block in provider.tf

--- provider.tf
+++ provider.tf
@@ -19,5 +19,6 @@ provider "helm" {
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
-*/
+
 }
+*/
