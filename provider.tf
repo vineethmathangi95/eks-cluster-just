0b83c2f7b298bc606a9d4a72b74c242b3930124e
vineeth mathangi <125802544+vineethmathangi95@users.noreply.github.com>
2026-03-18 15:15:39 +0530

    Uncomment Kubernetes provider and EKS data sources
    Uncomment Kubernetes provider configuration and related data sources.

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
-/*
+
 data "aws_eks_cluster" "cluster" {
   name = local.cluster_name
 }
@@ -6,7 +6,7 @@ data "aws_eks_cluster" "cluster" {
 data "aws_eks_cluster_auth" "cluster" {
   name = local.cluster_name
 }
-*/
+
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
