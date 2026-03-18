b3cc8d47002dc0f46eab520fc4d808c5519c42ed
vineeth mathangi <125802544+vineethmathangi95@users.noreply.github.com>
2026-03-18 14:58:41 +0530

    Comment out EKS cluster and Kubernetes provider
    Comment out Kubernetes provider configuration and EKS data sources.

--- provider.tf
+++ provider.tf
@@ -1,3 +1,4 @@
+/*
 data "aws_eks_cluster" "cluster" {
   name = local.cluster_name
 }
@@ -6,7 +7,7 @@ data "aws_eks_cluster_auth" "cluster" {
   name = local.cluster_name
 }
 
-/*
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
@@ -21,4 +22,5 @@ provider "helm" {
   }
 
 }
+
 */
