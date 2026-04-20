c82058b5da777867541503fdf5bcd537887c5d59
vineeth mathangi <125802544+vineethmathangi95@users.noreply.github.com>
2026-04-20 11:34:21 +0530

    Comment out helm provider configuration
    Comment out the helm provider configuration in terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -29,7 +29,7 @@ terraform {
       source  = "hashicorp/cloudinit"
       version = "~> 2.3.4" 
     }
-    
+    /*
     helm = {
       source  = "hashicorp/helm"
       version = "~> 2.7.0"
@@ -38,6 +38,7 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.11.0" 
    } 
+*/
   }
     required_version = "~> 1.3"
 }
