6f98b903ea6e82d79b26673a3c2e2464860469ad
vineeth mathangi <125802544+vineethmathangi95@users.noreply.github.com>
2026-04-20 11:21:09 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "corpitsoft-backend-state"
     key    = "terraform.tfstate"
-    region = "us-west-1"
+    region = "us-east-1"
     use_lockfile = true
   }
 }
