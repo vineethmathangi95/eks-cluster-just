7c917b4f2393a86ee38feac2085753e6a9f6ce6d
vineeth mathangi <125802544+vineethmathangi95@users.noreply.github.com>
2026-04-22 12:04:22 +0530

    Update aws-lb-controller.tf

--- aws-lb-controller.tf
+++ aws-lb-controller.tf
@@ -1,4 +1,4 @@
-/*
+
 module "load_balancer_controller" {
   source = "git::https://github.com/DNXLabs/terraform-aws-eks-lb-controller.git"
 
@@ -6,4 +6,4 @@ module "load_balancer_controller" {
   cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
   cluster_name                     = local.cluster_name
 }
-*/
+
